ac2fae39de6f2d6ed70c8efbed1f44104f380bc4
梁家文 <515521265@qq.com>
2017-06-05 12:05:43 +0800

    添加jwscrollview

---
+++ SwiftDemo/Other/JWScrollView.swift
@@ -0,0 +1,156 @@
+//
+//  JWScrollView.swift
+//  SwiftDemo
+//
+//  Created by 梁家文 on 2017/6/5.
+//  Copyright © 2017年 梁家文. All rights reserved.
+//
+
+import UIKit
+
+class JWScrollView: UIScrollView {
+
+    //定义block
+    typealias tapSelectRow = (NSInteger) -> ()
+    
+    public var allSubviews = Array<UIView>()
+    
+    public var paddingHeight = CGFloat()
+    
+    public var isGestureEnabled = Bool()
+    //block变量
+    public var resultBlock : tapSelectRow?
+
+    private var maxWidth = CGFloat()
+    
+    private var isAutomaticAdd = Bool()
+    
+    func setScrollerviewSubViewsArr(views:Array<UIView>)  {
+        
+        self.allSubviews = views
+        
+        self.reloadSubViews(views: self.allSubviews)
+    }
+    
+    
+    private func reloadSubViews(views:Array<UIView>)  {
+        
+        if views.count<=0 {
+            return
+        }
+        
+        self.isAutomaticAdd = true
+        
+        let firstView = views.first
+        firstView?.tag = 1992
+        //莫名其妙的偏移。。。处理一下
+        firstView?.y = -(firstView?.height)!
+        firstView?.x = (kScreenWidth - (firstView?.width)!)/2
+        
+        if self.isGestureEnabled {
+            
+            let firstTap = UITapGestureRecognizer.init(target: self, action: #selector(viewTapAction))
+            firstTap.numberOfTapsRequired=1
+            firstTap.numberOfTouchesRequired = 1
+            firstView?.addGestureRecognizer(firstTap)
+            
+        }
+        self.addSubview((firstView)!)
+        
+    
+        /*遍历需要传正向遍历还是倒叙遍历
+         enumerated() 正序
+         reversed() 倒叙
+         */
+        for (item,Vieww) in views.enumerated(){
+            let view = Vieww
+            view.tag = 1992 + item
+            view.x = (kScreenWidth - view.width)/2
+            if self.isGestureEnabled {
+                let tap = UITapGestureRecognizer.init(target: self, action: #selector(viewTapAction))
+                tap.numberOfTapsRequired=1
+                tap.numberOfTouchesRequired = 1
+                Vieww.addGestureRecognizer(tap)
+
+            }
+            view.y = (self.subviews.last?.y)! + (self.subviews.last?.height)!
+            self.addSubview(view);
+            
+            if item == views.count-1 {
+                
+                self.contentSize = CGSize.init(width: 0, height: (self.subviews.last?.y)! + (self.subviews.last?.height)! + self.paddingHeight)
+                
+            }
+            
+        }
+        
+    }
+    
+    func viewTapAction (tap:UITapGestureRecognizer)  {
+        
+        self.resultBlock!((tap.view?.tag)!-1992)
+        
+    }
+    
+    
+    override func layoutSubviews() {
+        
+        super.layoutSubviews()
+    
+    }
+    
+    
+    func removeViewWithTag(ViewTag:NSInteger)  {
+        
+        for (index, value) in self.allSubviews.enumerated() {
+            if value.tag == ViewTag {
+                value.removeFromSuperview()
+                self.allSubviews.remove(at: index)
+            }
+        }
+        
+        self.reloadSubViews(views: self.allSubviews)
+        
+    }
+    
+    func reloadViews()  {
+        
+        self.reloadSubViews(views: self.allSubviews)
+    }
+    
+    func removeAllSubViews()  {
+        
+        for (_,value) in self.subviews.enumerated() {
+            
+            value.removeFromSuperview()
+            
+        }
+        
+    }
+    
+    func allSubViewsClipsToBounds()  {
+        
+        for (_,value) in self.subviews.enumerated() {
+            
+            value.clipsToBounds = true
+            
+        }
+
+    }
+    
+    
+    override func addSubview(_ view: UIView) {
+        super.addSubview(view)
+        
+        if self.isAutomaticAdd {
+            return
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+}
--- SwiftDemo/ViewController/ProductViewController.swift
+++ SwiftDemo/ViewController/ProductViewController.swift
@@ -11,26 +11,45 @@ import UIKit
 class ProductViewController: BaseViewController {
 
     
-    let scrollerView = {
+    let scrollerView = { () -> JWScrollView in 
     
-        return UIScrollView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: kScreenHeight));
+        
+        let sv = JWScrollView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: kScreenHeight))
+        
+        sv.alwaysBounceVertical = true
+        sv.isGestureEnabled = true
+        return sv;
         
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        scrollerView.alwaysBounceHorizontal = true;
-        scrollerView.alwaysBounceVertical = true;
-        
+        //添加控件位置偏差
+//        self.automaticallyAdjustsScrollViewInsets = false;
+
         self.view.addSubview(scrollerView);
         
         
-        let lab = UILabel.init(frame: CGRect.init(x: 0, y: 140, width: kScreenWidth, height: 30));
-        lab.text = "label"
-        lab.textAlignment = NSTextAlignment.center;
-        scrollerView.addSubview(lab);
+        let lab1 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 90));
+        lab1.text = "label1"
+        lab1.textAlignment = NSTextAlignment.center;
+        lab1.backgroundColor = UIColor.red
         
+        let lab2 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 130));
+        lab2.text = "label2"
+        lab2.textAlignment = NSTextAlignment.center;
+        lab2.backgroundColor = UIColor.yellow
+        
+        let lab3 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 50));
+        lab3.text = "label3"
+        lab3.textAlignment = NSTextAlignment.center;
+        lab3.backgroundColor = UIColor.red
+        
+        self.scrollerView.setScrollerviewSubViewsArr(views: [lab1,lab2,lab3])
+        
+//        self.scrollerView.resultBlock = func (index :NSInteger){
+//        
+//        }
         
 
         
