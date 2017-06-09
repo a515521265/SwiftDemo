d829ed18d2a248b55751dac31c060a13f01bb24b
梁家文 <515521265@qq.com>
2017-06-09 14:56:17 +0800

    添加tabbar图片。

--- SwiftDemo/MainTabBarViewController.swift
+++ SwiftDemo/MainTabBarViewController.swift
@@ -8,22 +8,57 @@
 
 import UIKit
 
-class MainTabBarViewController: UITabBarController {
+class MainTabBarViewController: UITabBarController,UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        delegate = self as? UITabBarControllerDelegate;
+        
+        //先添加图片
+        self.tabBar.backgroundImage = UIImage()
+        self.tabBar.shadowImage = UIImage()
+        let backimageV = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 64))
+        backimageV.image = UIImage.init(named: "cm2_btm_bg")
+        self.tabBar.addSubview(backimageV)
+        
+        
+        delegate = self
         // Do any additional setup after loading the view.
         let nav1 = MainNavViewController.init(rootViewController: HomeViewController());
-        settabBar(nav: nav1, title: "推荐", image: "common-tuijianno", selectedImage: "common-tuijian")
+        settabBar(nav: nav1, title: "推荐", image: "cm2_btm_icn_account", selectedImage: "cm2_btm_icn_account_prs")
         let nav2 = MainNavViewController.init(rootViewController: ProductViewController());
-        settabBar(nav: nav2, title: "产品", image: "common-productno", selectedImage: "common-product")
+        settabBar(nav: nav2, title: "产品", image: "cm2_btm_icn_discovery", selectedImage: "cm2_btm_icn_discovery_prs")
         let nav3 = MainNavViewController.init(rootViewController: FindViewController());
-        settabBar(nav: nav3, title: "发现", image: "common-faxianno", selectedImage: "common-faxian")
+        settabBar(nav: nav3, title: "发现", image: "cm2_btm_icn_friend", selectedImage: "cm2_btm_icn_friend_prs")
         let nav4 = MainNavViewController.init(rootViewController: AccountViewController());
-        settabBar(nav: nav4, title: "账户", image: "common-accountno", selectedImage: "common-account")
+        settabBar(nav: nav4, title: "账户", image: "cm2_btm_icn_music", selectedImage: "cm2_btm_icn_music_prs")
         let nav5 = MainNavViewController.init(rootViewController: ViewController());
-        settabBar(nav: nav5, title: "更多", image: "common-moreno", selectedImage: "common-more")
+        settabBar(nav: nav5, title: "更多", image: "cm2_btm_icn_discovery", selectedImage: "cm2_btm_icn_discovery_prs")
+        
+
+        
+        
+        
+//        print(self.tabBar.subviews);
+//        
+//        let arrs = self.tabBar.subviews;
+//        
+//        
+//        for (_,item) in arrs.enumerated() {
+//            
+//            let cview = UIView.init(frame: item.frame);
+//            
+//            cview.backgroundColor = UIColor.red
+//            
+//            cview.width = kScreenWidth/5;
+//            
+//            cview.isUserInteractionEnabled = false;
+//            
+//            cview.alpha = 0.5;
+//            
+//            self.tabBar.addSubview(cview);
+//            
+//        }
+        
         
         
     }
@@ -33,7 +68,7 @@ class MainTabBarViewController: UITabBarController {
         self.addChildViewController(nav)
         
         nav.tabBarItem = UITabBarItem.init(title: title, image: UIImage.init(named: image), selectedImage: UIImage.init(named: selectedImage));
-        nav.tabBarItem.image = UIImage.init(named: image)
+        nav.tabBarItem.image = UIImage.init(named: image)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
         nav.tabBarItem.selectedImage = UIImage.init(named: selectedImage)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal);
         
         let textAttrs1 = NSMutableDictionary()
@@ -44,6 +79,15 @@ class MainTabBarViewController: UITabBarController {
         
         nav.tabBarItem.setTitleTextAttributes(textAttrs2 as? [String : Any], for: UIControlState.normal)
         nav.tabBarItem.setTitleTextAttributes(textAttrs1 as? [String : Any], for: UIControlState.selected)
+        
+        
+    }
+    
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+        
+        print(self.selectedIndex);
         
         
     }
