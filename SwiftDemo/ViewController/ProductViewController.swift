f5ccb0138377c1bf7090d237469fcbe9fb7580bb
梁家文 <515521265@qq.com>
2017-06-15 14:36:04 +0800

    scrollerView刷新bug修改。

--- SwiftDemo/ViewController/ProductViewController.swift
+++ SwiftDemo/ViewController/ProductViewController.swift
@@ -28,29 +28,20 @@ class ProductViewController: BaseViewController ,CirCleViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //添加控件位置偏差
-//        self.automaticallyAdjustsScrollViewInsets = false;
+        
         self.view.addSubview(scrollerView);
-        
-        
-//        let imageArray: [UIImage?] = [UIImage(named: "first.jpg"), UIImage(named: "second.jpg"), UIImage(named: "third.jpg")]
-        
+        //轮播图
         self.circleView = CirCleView(frame: CGRect(x: 0, y: 64, width: self.view.frame.size.width, height: 200), imageArray: [])
         circleView.backgroundColor = UIColor.orange
         circleView.delegate = self
-//        self.view.addSubview(circleView)
-        
-        delay(2)  {
+        delay(1)  {
             self.circleView.urlImageArray = ["http://a2.qpic.cn/psb?/V14fUnYP0SXDXU/RjNqTMAuvskdaYVmg3NkVlg*JK6owbqyD4.M*ql52ik!/b/dAQAAAAAAAAA&bo=cgcgA.MOQAYFCNY!&rf=viewer_4","http://a1.qpic.cn/psb?/V14fUnYP0SXDXU/1ZZAgz.VFZRKb8Cegre.eZuJJfhjjXjIhG8n.EahULk!/b/dAMAAAAAAAAA&bo=KwQgA0AGsAQFAN0!&rf=viewer_4","http://a2.qpic.cn/psb?/V14fUnYP0SXDXU/xwT4aLFAO.gT32CKLU0o6Q0SmMjmcM0v8WXTb2M6gwI!/b/dAQAAAAAAAAA&bo=KwQgA0AGsAQFAN0!&rf=viewer_4"]
         }
-        
         var arr = Array<UIView>()
-        
         arr.append(circleView)
-        
-        for _ in 1...15 {
+        for i in 1...15 {
             let cells = JWScrollviewCell.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 50))
-            cells.leftLabel().text = "账号"
+            cells.leftLabel().text = "账号".appending(String(i))
             cells.rightTextField().placeholder = "请输入您的账号"
             cells.setLineColor(lineColor: UIColor.groupTableViewBackground)
             cells.setUPandDownSpacing(upSpacing: 0.5, downSpacing: 0.5)
@@ -59,62 +50,34 @@ class ProductViewController: BaseViewController ,CirCleViewDelegate{
         self.scrollerView.setScrollerviewSubViewsArr(views: arr)
         
         
-        
         let footer = RHRefreshFooter(refreshScrollView: scrollerView)
         footer.refreshHandler = {
-            
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3.0) {
-
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
                 footer.endRefresh()
-
-                    footer.hasMoreData = false
-                
+                arr.remove(at: 1)
+                self.scrollerView.removeViewWithTag(ViewTag: 1993)
             }
-            
         }
         
         let header = RHRefreshHeader(refreshScrollView: scrollerView)
         header.refreshHandler = {
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3.0) {
-
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
                 header.endRefresh()
-                
-                footer.hasMoreData = true
+                let rView = UIView()
+                rView.frame = CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 50)
+                rView.backgroundColor = getRandomColor()
+                arr.insert(rView, at: 1)
+                self.scrollerView.allSubviews = arr
+                UIView.animate(withDuration: 0.5, animations: { 
+                    self.scrollerView.reloadViews()
+                }, completion: nil)
+//                footer.hasMoreData = true
             }
+            
         }
-        header.beginRefresh()
-        
-        
-        
-//        let lab1 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 90));
-//        lab1.text = "label1"
-//        lab1.textAlignment = NSTextAlignment.center;
-//        lab1.backgroundColor = UIColor.red
-//        
-//        
-//        let lab2 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 130));
-//        lab2.text = "label2"
-//        lab2.textAlignment = NSTextAlignment.center;
-//        lab2.backgroundColor = UIColor.yellow
-//        
-//        let lab3 = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: 50));
-//        lab3.text = "label3"
-//        lab3.textAlignment = NSTextAlignment.center;
-//        lab3.backgroundColor = UIColor.red
-        
-//        self.scrollerView.setScrollerviewSubViewsArr(views: [lab1,lab2,lab3,cell1])
-        
-
-//        self.scrollerView.tapSelectRow = {(row) in
-//            
-//            print(row);
-//            
-//            self.view.endEditing(true)
-//            
-//        }
-
-        
-        
+        //进入界面手动调用下拉刷新
+//        header.beginRefresh()
         
     }
     
